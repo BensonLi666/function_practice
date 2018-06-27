7c6863dde21b29500a2abd076ac1837eca7be920
BensonLi666 <34561928+BensonLi666@users.noreply.github.com>
2018-06-27 21:16:06 +0800

    Add files via upload

---
+++ function_practice.py
@@ -0,0 +1,41 @@
+print question 1 
+def f(x):
+    return 6 * x - 4
+print(f(4))
+print(f(5))
+print(f(6))
+print(f(7))
+
+print question 2
+def f(n):
+    return = 1 / (3**n-1)
+print(f(1))
+print(f(2))
+print(f(3))
+print(f(4))
+print(f(5))
+
+print question 3
+def f(n):
+	return = (-1**n) * (3)
+print(f(1))
+print(f(2))
+print(f(3))
+print(f(4))
+print(f(5))
+
+worksheet
+print question a
+
+def m(h):
+	return = 1000 + (100 * h)
+
+print question b
+
+def b(m):
+	return = m / 20
+
+print question c
+
+def b(m(k)):
+	return = [ 1000 + (100 * k) ] / 20
